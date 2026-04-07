0f8984f33271f30e15345baae242743bf867aa05
plebbian <loganf@nmsu.edu>
2026-04-06 21:04:10 -0600

    docker compose working

--- infra/docker/entrypoint.sh
+++ infra/docker/entrypoint.sh
@@ -4,8 +4,6 @@ set -e
 INTERFACE="${HONEYD_INTERFACE:-lo}"
 CONFIG="${HONEYD_CONFIG:-/opt/honeyd/honeyd.conf}"
 HONEYD_NET="${HONEYD_NET:-192.168.0.0/24}"
-ROUTE_NET="${ROUTE_NET:-192.168.0.0}"
-ROUTE_MASK="${ROUTE_MASK:-255.255.255.0}"
 HONEYD_GW="${HONEYD_GW:-127.0.0.1}"
 HONEYD_BIN="${HONEYD_BIN:-/usr/bin/honeyd}"
 
@@ -24,7 +22,7 @@ if [ ! -f "${CONFIG}" ]; then
     exit 1
 fi
 
-echo "[*] ADDING ROUTE: ${ROUTE_NET}/${ROUTE_MASK} via ${HONEYD_GW}"
+echo "[*] ADDING ROUTE: ${HONEYD_NET} via ${HONEYD_GW}"
 ip route add "${HONEYD_NET}" via "${HONEYD_GW}" 2>/dev/null || true
 
 echo "[*] STARTING HONEYD..."
