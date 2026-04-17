23a5122e1f53022753c726ae80d12cce0e9ace44
TomaHawk2005 <tchatto@nmsu.edu>
2026-04-17 00:45:36 -0600

    Update scenarios and fix empty service scripts

---
+++ scenarios/sample_services/http.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+echo "HTTP/1.0 200 OK"
+echo "Content-Type: text/html"
+echo "Server: Apache/2.4.41"
+echo ""
+echo "<html><body><h1>Welcome</h1></body></html>"
+sleep 30
---
+++ scenarios/sample_services/ssh.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+echo "SSH-2.0-OpenSSH_7.4"
+sleep 30
